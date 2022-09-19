f4d1eee30ab246cca52b34d36a83c2e5e9eb6094
RandyCheBro <kolyavolya@yandex.ru>
2022-09-20 00:59:56 +0300

    bug_index1.css

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,22 @@
 const popupOpen = document.querySelector(".profile__edit-button");
 const popupEle = document.querySelector(".popup");
 const popupClose = document.querySelector(".popup__close");
+const popupCloseOutside = document.querySelector(".popup__container");
 
 function openPopup() {
-  popupEle.classList.add(".popup_is-opened");
+  popupEle.classList.add("popup_is-opened");
 }
 
 function closePopup() {
-  popupEle.classList.remove(".popup_is-opened");
+  popupEle.classList.remove("popup_is-opened");
 }
 
 popupOpen.addEventListener("click", openPopup);
 popupClose.addEventListener("click", closePopup);
+popupCloseOutside.addEventListener("click", closePopup);
+popupCloseOutside.addEventListener("click", function(event) {
+  console.log("кликнули по:", event.target);
+  if (event.target === event.currentTarget) {
+    closePopup();
+  }
+});
