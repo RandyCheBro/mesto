7702b800a503416141b82633affe6be6b5ce76f3
RandyCheBro <kolyavolya@yandex.ru>
2022-10-19 14:19:02 +0300

    refactoring js

---
+++ scripts/cards.js
@@ -0,0 +1,32 @@
+const elements = [
+  {
+    name: "Рио-де-Жанейро",
+    link: "./images/rio.jpg",
+    alt: "Рио-де-Жанейро",
+  },
+  {
+    name: "Лондон",
+    link: "./images/london.jpg",
+    alt: "лондон",
+  },
+  {
+    name: "Сингапур",
+    link: "./images/Singapore.jpg",
+    alt: "сингапур",
+  },
+  {
+    name: "Нью-Йорк",
+    link: "./images/new-york.jpg",
+    alt: "Нью-Йорк",
+  },
+  {
+    name: "Санкт-Петербург",
+    link: "./images/sankt-petersburg.jpg",
+    alt: "Санкт-Петербург",
+  },
+  {
+    name: "Париж",
+    link: "./images/paris.jpg",
+    alt: "Париж",
+  },
+];
