cfca5d7b27cbc09918fee029477380bb3450bdda
RandyCheBro <kolyavolya@yandex.ru>
2022-12-07 13:55:35 +0300

    class userinfo

--- components/ModalWithForm.js
+++ components/ModalWithForm.js
@@ -1,14 +1,36 @@
 import { Modal } from "./Modal.js";
 
 export class ModalWithForm extends Modal {
-  _handleSubmit
-  
-  constructor({ handleSubmit }, modalSelector) {
+  _handleFormSubmit
+  _modalForm
+  constructor({ handleFormSubmit }, modalSelector) {
     super(modalSelector);
-    this._handleSubmit = handleSubmit;
+    this._handleFormSubmit = handleFormSubmit;
+    this._modalForm = this._modalSelector.querySelector(".popup__form");
   }
 
   _getInputValues() {
+    this._InputList = this._modalForm.querySelectorAll(".popup__input");
+    this._formValues = {};
 
+    this._InputList.forEach(input => {
+      this._formValues[input.name] = input.value;
+    })
+
+    return this._formValues;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+
+    this._modalForm.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+    })
+  }
+
+  close() {
+    super.close();
+    this._modalForm.reset()
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import {Card} from "../components/Card.js";
-import {FormValidator} from "../components/FormValidator.js";
-import {Section} from "../components/Section.js";
-import {Modal} from "../components/Modal.js";
-import {ModalWithImage} from "../components/ModalWithImage.js";
-import {ModalWithForm} from "../components/ModalWithForm.js";
+import { Card } from "../components/Card.js";
+import { FormValidator } from "../components/FormValidator.js";
+import { Section } from "../components/Section.js";
+import { Modal } from "../components/Modal.js";
+import { ModalWithImage } from "../components/ModalWithImage.js";
+import { ModalWithForm } from "../components/ModalWithForm.js";
+import { UserInfo } from "../components/UserInfo.js";
 import {
   elements,
   validation,
@@ -30,88 +31,57 @@ import {
 const cardList = new Section({
   renderer: (element => {
     const card = new Card(templateSelector, element, openModalCard);
-    const CardElement = card.create();
-    cardList.addItem(CardElement);
+    const cardElement = card.create();
+    cardList.addItem(cardElement);
   })
 }, elementList)
-
 cardList.renderItems(elements);
 
-/* function openModal(item) {
-  item.classList.add("popup_is-opened");
-  document.addEventListener("keydown", handleEscape);
-} */
-
-/* function closeModal(item) {
-  item.classList.remove("popup_is-opened");
-  document.removeEventListener("keydown", handleEscape);
-} */
-
-function openModalEditingProfile() {
-  inputProfileName.value = popupTitle.textContent;
-  inputProfileJob.value = popupSubTitle.textContent;
-  openModal(modalEditProfile);
-}
-
-function openModalAddingProfile() {
-  openModal(modalAddCard);
-  formValidatorAddingCard.resetValidation();
-}
-
-function handleSubmitProfileForm(evt) {
-  evt.preventDefault();
-  popupTitle.textContent = inputProfileName.value;
-  popupSubTitle.textContent = inputProfileJob.value;
-  closeModal(modalEditProfile);
-}
-
-function handleSubmitCreateCard(evt) {
-  evt.preventDefault();
-  const newNameCard = inputCardName.value
-  const newLinkCard = inputCardLink.value
-  const newCard = createCard({
-    link: newLinkCard,
-    alt: newNameCard,
-    name: newNameCard
-  });
-  evt.target.reset();
-  elementList.prepend(newCard);
-  closeModal(modalAddCard);
-}
 
 const modalImage = new ModalWithImage(modalCardImage, modalCardName, modalPreviewImage)
 modalImage.setEventListeners();
 
-function openModalCard(photoData) {
-  /* modalCardImage.src = photoData.link;
-  modalCardImage.alt = photoData.alt;
-  modalCardName.textContent = photoData.name; */
-  modalImage.open(photoData)
-  /* openModal(modalPreviewImage); */
+
+const modalAddingCard = new ModalWithForm({
+  handleFormSubmit: (formData) => {
+    cardList.addItem(createCard({
+      link: formData['image-link'],
+      name: formData['image-name']
+    }));
+    modalAddingCard.close();
+  }
+}, modalAddCard)
+modalAddingCard.setEventListeners();
+
+
+const profileInfo = new UserInfo({nameSelector: popupTitle, jobSelector: popupSubTitle});
+
+const modalEditingProfile = new ModalWithForm({
+  handleFormSubmit: (formData) => {
+    profileInfo.setUserInfo(formData["profile-name"], formData["profile-job"])
+    modalEditingProfile.close();
+  }
+}, modalEditProfile)
+modalEditingProfile.setEventListeners();
+
+
+function openModalEditingProfile() {
+  modalEditingProfile.open()
+  formValidatorEditingProfile.resetValidation();
+  const {name, description} = profileInfo.getUserInfo();
+  inputProfileName.value = name;
+  inputProfileJob.value = description;
 }
 
-btnEditingProfile.addEventListener('click', openModalEditingProfile)
-btnAddingCard.addEventListener('click', openModalAddingProfile);
-formEditingProfile.addEventListener("submit", handleSubmitProfileForm);
-formAddingCard.addEventListener("submit", handleSubmitCreateCard);
+function openModalAddingProfile() {
+  formValidatorAddingCard.resetValidation();
+  modalAddingCard.open();
+}
 
-/* modals.forEach((modal) => {
-  modal.addEventListener("mousedown", (evt) => {
-    if(evt.target.classList.contains("popup_is-opened")) {
-      closeModal(modal);
-    }
-    if(evt.target.classList.contains("popup__close")) {
-      closeModal(modal);
-    };
-  });
-});
+function openModalCard(photoData) {
+  modalImage.open(photoData)
+}
 
-const handleEscape = (evt) => {
-  if(evt.key === "Escape") {
-    const popup = document.querySelector(".popup_is-opened");
-    closeModal(popup);
-  };
-} */
 
 const createCard = (element) => {
   const card = new Card(templateSelector, element, openModalCard);
@@ -119,10 +89,8 @@ const createCard = (element) => {
   return cardElement;
 }
 
-/* elements.forEach(function (item) {
-  const cardElement = createCard(item);
-  elementList.append(cardElement);
-}); */
+btnEditingProfile.addEventListener('click', openModalEditingProfile)
+btnAddingCard.addEventListener('click', openModalAddingProfile);
 
 const formValidatorEditingProfile = new FormValidator(validation, formEditingProfile);
 formValidatorEditingProfile.enableValidation();
